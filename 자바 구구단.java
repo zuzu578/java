44312bb7c588231c275bd26321908ccc2db1476f
Turltes And Cats <69393030+dlwnghks6821@users.noreply.github.com>
2020-10-06 21:24:04 +0900

    java 구구단.

---
+++ 자바 구구단.java
@@ -0,0 +1,17 @@
+
+
+
+public class f15 {
+
+	public static void main(String[] args) {
+             int i = 0;
+             int a = 0;
+             for(i = 0; i<10; i++) {
+            	 System.out.println(i);
+            	 for(a = 0; a<10 ; a++);
+            	 System.out.println(a);
+             }
+	   }
+		}
+
+	
