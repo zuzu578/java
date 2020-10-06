5aadb8ffe26961894c9fba5cd70645ae67212593
Turltes And Cats <69393030+dlwnghks6821@users.noreply.github.com>
2020-10-06 17:09:35 +0900

    자바삼항연산자, 구구단
    자바 기본문법 증감 연산자 ++ -- 를 이용한 구구단입니다.

---
+++ java1.java
@@ -0,0 +1,31 @@
+package project1;
+
+public class project {
+   
+	public static void main(String[] args) {
+		/*
+	String name ="dlwnghks";
+	String birth="20000529";
+	String address="korea";
+	System.out.println(name+"\n"+birth+"\n"+address);
+	*/
+        int n = 2;
+        int a = 1;
+        System.out.println(n+" x "+a+" = "+(n*a));
+        ++a;
+        System.out.println(n+" x "+a+" = "+(n*a));
+        ++a;
+        System.out.println(n+" x "+a+" = "+(n*a));
+        ++a;
+        System.out.println(n+" x "+a+" = "+(n*a));
+        ++a;
+        System.out.println(n+" x "+a+" = "+(n*a));
+        ++a;
+   
+  
+   
+	
+	}
+	
+
+}
