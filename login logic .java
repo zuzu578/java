24c70f6761ccea6d7e673aa65358f7fecd28a5a7
Turltes And Cats <69393030+dlwnghks6821@users.noreply.github.com>
2020-10-06 22:39:49 +0900

    java- login logic

---
+++ login logic .java
@@ -0,0 +1,14 @@
+
+public class javascript {
+
+	public static void main(String[] args) {
+	  String id ="dlwnghks6821";
+	  String pwd ="1111";
+	  if(id == "dlwnghk") {
+		  System.out.println("please input your password");
+	  }else {
+		  System.out.println("your password not working our logics");
+	  }
+	}
+
+}
