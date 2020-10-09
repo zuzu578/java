547410a477a1454a1094ba10b6d7d9d7a7502128
Turltes And Cats <69393030+dlwnghks6821@users.noreply.github.com>
2020-10-09 16:35:03 +0900

    팩토리얼 계산기.
    1. 사용방 법. 
    스캐너 를 썻으므로 
    콘솔창에 원하는 숫자를 넣는다.
    
    2. 스캐너를 통해서 알려주게된다 .

---
+++ 입력받은 숫자에 대한 팩토리얼 값 .java
@@ -0,0 +1,26 @@
+ Scanner sc = new Scanner(System.in);
+      
+      System.out.println("숫자를 입력해주세요.");
+      int s = sc.nextInt();
+       int p = 1; 
+       
+       
+      
+      
+      for(int i= s ;  s >0;  s --) {
+    	  p = s*p ; 
+    	  System.out.println(p);
+      }
+	} 
+		   
+			   
+	 
+
+		 
+		 
+		   
+		}
+		
+	
+		
+	
