537c60efd85a8733d46f356ff0b3eec8c0fcd8e1
Turltes And Cats <69393030+dlwnghks6821@users.noreply.github.com>
2020-10-16 16:15:54 +0900

    method

---
+++ 함수 .java
@@ -0,0 +1,49 @@
+package cf1;
+
+public class cf {
+	
+	
+
+	int   p=0;
+	
+
+	public static void main(String[] args) {
+	      
+		/// int n=	(int)Math.pow(4, 3);
+	 //System.out.println(n);
+	   
+	 //int m =(int) Math.sqrt(16);
+	 //System.out.println(m);
+		
+		int a = 3 ;
+		int b =4 ;
+	    pita(a,b);
+		
+	 
+ }
+
+	public static void   pita(int a, int b) {
+		 int i  = (int)Math.pow(a, 2);
+		int  j = (int)Math.pow(b,2);
+	      int k = (int)Math.sqrt(i+j);
+	     int  p = k;
+	   
+		System.out.println(p);
+
+	}
+	
+	
+	
+	
+
+
+
+
+
+	             
+	}
+
+
+	
+	
+	
