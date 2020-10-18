51049d89177cfc963e0f007d326c2184db0ae464
Turltes And Cats <69393030+dlwnghks6821@users.noreply.github.com>
2020-10-18 22:14:30 +0900

    복습

---
+++ 복습 .1.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+
+public class NewCalCulator {
+	
+
+	public static void main(String[] args) {
+		
+		//기존의 배열방법 //
+		/*int []a = new int[30];
+	// int[]a = new int[];
+	//a[0]=100;
+		//a[1]=101;
+		//a[2]=102;
+		//a[3]=103;
+		//a[4]=104;
+		//a[29]=129;
+		
+		for(int i = 0 ; i<=29 ; i++) {
+			a[i] = 100+i;
+		
+		}
+		
+		
+		for(int j = 0 ; j<=a.length ; j++) {
+			a[j] = 100+j;
+			System.out.println("a"+j+a[j]);
+			
+		}*/
+	
+		
+		
+		
+		//최신의 배열방법 (ArrayList)//
+		
+		/*ArrayList a = new ArrayList();
+		
+		for(int i = 0; i<=29 ; i++){
+			a.add(i+100);
+			
+			
+		}
+		
+		System.out.println(a);*/
+		
+		
+		//1부터 40까지 수중에 20이상 30이하의 수를 제외하고  //
+	    // 나머지 숫자를 출력해라 .//
+		// 그리고나머지 숫자의 합을 구해라//
+		
+
+		/*int sum = 0 ;
+		 
+		for( int i = 1 ;i<=40 ; i++){
+			if(i>=20 && i<=30) {
+		
+				sum=sum+i;
+				continue;
+				
+				
+			}
+			System.out.println(sum);
+
+			
+			
+			
+			
+		}*/
