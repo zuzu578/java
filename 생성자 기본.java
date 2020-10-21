625cfde405e23ff7ee7572a1dd18ea60ad65badd
Turltes And Cats <69393030+dlwnghks6821@users.noreply.github.com>
2020-10-21 20:34:23 +0900

    생성자

---
+++ 생성자 기본.java
@@ -0,0 +1,24 @@
+
+
+
+
+
+public class Example {
+            //생성자인데 인자 값이 있는 생성자 //
+	        //인자 값이 있는 생성자는 반드시 인스턴스에서 인자값을 입력해줘야함//
+            Example(int x){
+            	
+            }
+            //기본 생성자 (인자가 없는 기본 생성자)//
+            Example(){
+            	
+            }
+
+	public static void main(String[] args) {
+	  Example e = new Example();
+	}
+
+}
+
+
+
