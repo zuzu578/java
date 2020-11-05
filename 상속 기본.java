3abf28c836c2bcae320cceb621aed2a1feab0293
Napalm <69393030+dlwnghks6821@users.noreply.github.com>
2020-11-05 23:12:53 +0900

    extends

---
+++ 상속 기본.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class testfor {
+	
+
+	public static void main(String[] args) {
+     Calculator c1 = new Calculator(10,20);
+     c1.sum();
+     c1.avg();
+ 
+	}
+
+}
+class Calculator{
+	
+	int left, right;
+	//생성자의 역할//
+	
+	//1)초기에 해야할 작업을(초기화 작업을)해준다//
+	//2)초기값들을 매개변수로 받아줘서 초기화 작업을 해준다//
+	//3)클래스 인스턴스화 하고 그 인스턴스화할때 매개변수를 써놓으면 그매개변수를 받아주는 역할도한다//
+	//4)결론 ==>초기화할 값을 받고 그리고 생성자라는 공간안에서 초기화작업을한다.//
+	
+	Calculator(int x ,int y){
+	//매개변수가 있는 생성자 //
+	//기본생성자X//
+	
+	}
+	public void avg() {
+		// TODO Auto-generated method stub
+		
+	}
+	public void sum() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+	
+}
